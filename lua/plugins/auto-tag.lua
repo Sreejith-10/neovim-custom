744c7fd407a1a8a0298d56c1a7895c659ce83d8d
sreejith <sreejithsree956266@gmail.com>
2026-01-01 22:50:02 +0530

    auto tag warning fix

--- lua/plugins/auto-tag.lua
+++ lua/plugins/auto-tag.lua
@@ -2,10 +2,21 @@ return {
 	-- autoclose tags
 	"windwp/nvim-ts-autotag",
 	ft = { "html", "typescriptreact", "jsx", "tsx" },
-	opts = {
-		-- Defaults
-		enable_close = true, -- Auto close tags
-		enable_rename = true, -- Auto rename pairs of tags
-		enable_close_on_slash = false, -- Auto close on trailing </
-	},
+	config = function()
+		require("nvim-ts-autotag").setup({
+			filetypes = {
+				"html",
+				"jsx",
+				"tsx",
+				"javascriptreact",
+				"typescriptreact",
+			},
+			autotag = {
+				enable = true,
+				enable_rename = true,
+				enable_close = true,
+				enable_close_on_slash = false,
+			},
+		})
+	end,
 }
